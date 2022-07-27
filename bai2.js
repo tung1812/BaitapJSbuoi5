fe8b6af567ce26b9c14d7ff45d5db565fe77773e
tung1812 <binlabum123@gmail.com>
2022-07-27 23:31:18 +0700

    fixed

--- bai2.js
+++ bai2.js
@@ -5,25 +5,30 @@ function elecBill() {
     kw = parseInt(kw);
 
     // Xu ly
-    var tienDien = 0;
-    
-    if ((kw > 0) && (kw <= 50)) {
-        tienDien = 500;
+    if (kw < 0) {
+        alert("Khong hop le")
     }
-    else if ((kw > 50) && (kw <= 100)) {
-        tienDien = 650;
-    }
-    else if ((kw > 100) && (kw <= 200)) {
-        tienDien = 850;
-    }
-    else if ((kw > 200) && (kw <= 350)) {
-        tienDien = 1100;
-    }
-    else if (kw > 350) {
-        tienDien = 1300;
-    }
+    else {
+        var tienDien = 0;
+        if ((kw > 0) && (kw <= 50)) {
+            tienDien = 500;
+        }
+        else if ((kw > 50) && (kw <= 100)) {
+            tienDien = 650;
+        }
+        else if ((kw > 100) && (kw <= 200)) {
+            tienDien = 850;
+        }
+        else if ((kw > 200) && (kw <= 350)) {
+            tienDien = 1100;
+        }
+        else if (kw > 350) {
+            tienDien = 1300;
+        }
 
-    var tongTien = kw * tienDien;
-    var result = "Ho va ten:" + " " + hoTen + ". So tien:" + " " + tongTien + "VND";
-    document.getElementById("footer2").innerHTML = result;
+        var tongTien = kw * tienDien;
+        var result = "Ho va ten:" + " " + hoTen + ". So tien:" + " " + tongTien + "VND";
+        document.getElementById("footer2").innerHTML = result;
+    }
+    
 }
